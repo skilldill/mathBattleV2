fd9ce3b265a50a3990b5547b358ff5f777fdefe5
Alexander <skilldill@yandex.ru>
2025-04-20 18:54:37 +0300

    Added client

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import MathBattleCoverPNG from './assets/math-battle-cover.png';
+
+function App() {
+  return (
+    <>
+      <div className="cover-image-container">
+        <img src={MathBattleCoverPNG} alt="Math Battle Cover" />
+      </div>
+      <h1>Math Battle comming soon</h1>
+    </>
+  )
+}
+
+export default App
