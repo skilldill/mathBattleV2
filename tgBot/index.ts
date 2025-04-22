c2114460b4589883f6b30ff88d06301835652553
Alexander <skilldill@yandex.ru>
2025-04-22 22:03:06 +0300

    Added tgBot

---
+++ tgBot/index.ts
@@ -0,0 +1,18 @@
+import { Telegraf } from 'telegraf';
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx) => {
+  ctx.reply('Запусти Math Battle', {
+    reply_markup: {
+      keyboard: [[{
+        text: 'Открыть Math Battle',
+        web_app: { url: 'https://app.math-battle.ru' }
+      }]],
+      resize_keyboard: true
+    }
+  })
+})
+
+bot.launch()
+console.log('🤖 Бот работает!')
