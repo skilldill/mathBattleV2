c16715878eeaec5b80f5e41b18a51ea80104f76c
Alexander <skilldill@yandex.ru>
2025-04-24 19:04:26 +0300

    Updates for bot

--- bot/index.js
+++ bot/index.js
@@ -5,16 +5,47 @@ config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+const START_MESSAGE = `
+👋 Привет! Это Math Battle — арена для настоящих гениев чисел!
+
+Сейчас ты можешь:
+• 🧠 Тренироваться в решении примеров на скорость и точность
+
+• 📊 Следить за своими личными результатами  
+
+• 🏆 Совсем скоро — участвовать в ежедневных рейтингах  
+
+• 🧮 И главное — доказать, что ты настоящий математик!
+
+🚀 Готов начать?
+
+Жми кнопку ниже и вступай в игру!`;
+
+const getUrlWebApp = (userId, username) => `https://app.math-battle.ru?u91x=${userId}&x_3z9=${username}`;
+
+
 bot.start((ctx) => {
-  ctx.reply('Запусти Math Battle', {
+  ctx.reply(START_MESSAGE, {
     reply_markup: {
-      keyboard: [[{
-        text: 'Открыть Math Battle',
-        web_app: { url: `https://app.math-battle.ru?u91x=${ctx.from.id}&x_3z9=${ctx.from.username}` }
+      inline_keyboard: [[{
+        text: 'ПОЛЕТЕЛИ!',
+        web_app: { url: getUrlWebApp(ctx.from.id, ctx.from.username) }
       }]],
       resize_keyboard: true
     }
   })
+
+  setTimeout(() => {
+    ctx.reply('А ты точно математик? Сколько будет 2 + 2 * 2?', {
+      reply_markup: {
+        keyboard: [[{
+          text: '🔥 Доказать, кто тут математик!',
+          web_app: { url: getUrlWebApp(ctx.from.id, ctx.from.username) }
+        }]],
+        resize_keyboard: true
+      }
+    })
+  }, 3000)
 })
 
 bot.launch()
