1721098a74b07b58fef781359be7a27f8ca0a6f1
Alexander <skilldill@yandex.ru>
2025-04-24 11:33:48 +0300

    Add tg sdk react

--- client/src/screens/TechDevScreen.tsx
+++ client/src/screens/TechDevScreen.tsx
@@ -1,15 +1,19 @@
-import { useLaunchParams } from '@telegram-apps/sdk-react';
+import { useLaunchParams, useRawInitData } from '@telegram-apps/sdk-react';
 import { ScreenLayout } from '../components';
 
 
 export const TechDevScreen: React.FC = () => {
     const tgData = useLaunchParams();
+    const rawInitData = useRawInitData();
 
     return (
         <ScreenLayout>
             <h1>TechDevScreen</h1>
             <code>
                 {JSON.stringify(tgData, null, 2)}
+                <br />
+                <br />
+                {rawInitData}
             </code>
         </ScreenLayout>
     );
