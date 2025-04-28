e93aa4c7730ad77e19f8577e9a22f448d5f66b5e
Alexander <skilldill@yandex.ru>
2025-04-28 10:00:22 +0300

    commented cpde

--- bot/index.js
+++ bot/index.js
@@ -1,27 +1,27 @@
 const { config } = require('dotenv');
 const { Telegraf } = require('telegraf');
 const { START_MESSAGE_MAP, QUESTION_MESSAGE_MAP, BUTTON_MESSAGE_MAP, MESSAGE_BUTTON_TEXT } = require('./constants');
-const mongoose = require('mongoose');
-const { ResultModel } = require('./dbModels');
+// const mongoose = require('mongoose');
+// const { ResultModel } = require('./dbModels');
 
 config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const ADMIN_ID = process.env.ADMIN_ID;
 
-export async function connectDB() {
-  try {
-    console.log('Connecting to MongoDB', process.env.MONGODB_URI);
-    const mongoUrl = process.env.MONGODB_URI || 'mongodb://mongodb:27017/math';
-    await mongoose.connect(mongoUrl);
-    console.log('📦 Connected to MongoDB');
-  } catch (error) {
-    console.error('Failed to connect to MongoDB:', error);
-    process.exit(1);
-  }
-}
-// Вызываем функцию подключения
-connectDB();
+// export function connectDB() {
+//   try {
+//     console.log('Connecting to MongoDB', process.env.MONGODB_URI);
+//     const mongoUrl = process.env.MONGODB_URI || 'mongodb://mongodb:27017/math';
+//     mongoose.connect(mongoUrl);
+//     console.log('📦 Connected to MongoDB');
+//   } catch (error) {
+//     console.error('Failed to connect to MongoDB:', error);
+//     process.exit(1);
+//   }
+// }
+// // Вызываем функцию подключения
+// connectDB();
 
 const getUrlWebApp = (userId, username) => `https://app.math-battle.ru?u91x=${userId}&x_3z9=${username}`;
 
@@ -62,32 +62,32 @@ bot.start((ctx) => {
   }, 3000)
 })
 
-bot.command('users', async (ctx) => {
-  if (ctx.from.id !== ADMIN_ID) return;
+// bot.command('users', async (ctx) => {
+//   if (ctx.from.id !== ADMIN_ID) return;
 
-  try {
-    const uniquePlayers = await ResultModel.aggregate([
-      {
-        $group: { _id: "$userId" }
-      },
-      {
-        $project: { _id: 0, userId: "$_id" }
-      },
-      { $sort: { userId: 1 } } // сортируем по алфавиту (опционально)
-    ]);
+//   try {
+//     const uniquePlayers = await ResultModel.aggregate([
+//       {
+//         $group: { _id: "$userId" }
+//       },
+//       {
+//         $project: { _id: 0, userId: "$_id" }
+//       },
+//       { $sort: { userId: 1 } } // сортируем по алфавиту (опционально)
+//     ]);
 
-    if (uniquePlayers.length === 0) {
-      return ctx.reply('Пока что нет игроков.');
-    }
+//     if (uniquePlayers.length === 0) {
+//       return ctx.reply('Пока что нет игроков.');
+//     }
 
-    const usersList = uniquePlayers.map((u, idx) => `${idx + 1}. ${u.userId}`).join('\n');
+//     const usersList = uniquePlayers.map((u, idx) => `${idx + 1}. ${u.userId}`).join('\n');
 
-    await ctx.reply(`👥 Уникальные игроки:\n\n${usersList}`);
-  } catch (error) {
-    console.error('Ошибка при получении уникальных игроков:', error);
-    await ctx.reply('Ошибка при получении списка игроков. Попробуйте позже!');
-  }
-})
+//     await ctx.reply(`👥 Уникальные игроки:\n\n${usersList}`);
+//   } catch (error) {
+//     console.error('Ошибка при получении уникальных игроков:', error);
+//     await ctx.reply('Ошибка при получении списка игроков. Попробуйте позже!');
+//   }
+// })
 
 bot.launch()
 console.log('🤖 Math Battle Bot работает!')
