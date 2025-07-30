a9e86c49718d80e62d7a4eaa0c7a53e7a4013c6e
Alexander <skilldill@yandex.ru>
2025-07-30 22:18:39 +0300

    Update environment variables in package.json and vite.config.ts for development and production builds. Adjust API proxy target based on NODE_ENV to streamline local and production setups.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,8 @@ import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
+console.log('process.env.NODE_ENV', process.env.NODE_ENV)
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -18,9 +20,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://math-battle.ru',
+        target: process.env.NODE_ENV === 'production' ? 'https://math-battle.ru/' : 'http://localhost:4000/',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
+        rewrite: (path) => path.replace(/^\/api/, process.env.NODE_ENV === 'production' ? '/api' : '/'),
       },
     },
   },
